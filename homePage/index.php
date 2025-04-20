65959b0273a36927f0e249ea161ea0ead619b5c3
Luiza <luizamarianahg@hotmail.com>
2025-04-20 10:55:08 -0300

    attObjetivoChamado

--- homePage/index.php
+++ homePage/index.php
@@ -31,35 +31,30 @@
                 </ul>
             </nav>
         </div>
-        <div class="container-definicao d-flex align-items-center justify-content-between mt-5">
+        <div class="d-flex align-items-center justify-content-between mt-5">
             <div class="mx-5 px-3" style="width: 40%; border-left: 5px solid #131C63">
                 <h2 class="mb-3" style="font-weight: bolder;">O que é Cuidado Paliativo?</h2>
-                <p class="m-0" style="color: #131C63;">
-                    Cuidados paliativos são um conjunto de práticas voltadas para melhorar a qualidade de vida de pacientes com doenças graves ou incuráveis.<br><br>
-                    O foco não é a cura, mas o alívio do sofrimento, controle da dor e apoio emocional para o paciente e sua família.<br><br>
-                    Esse tipo de cuidado pode ser oferecido em qualquer fase da doença, junto a outros tratamentos, e envolve uma equipe multidisciplinar que inclui médicos, enfermeiros, psicólogos e assistentes sociais.
+                <p class="m-0" style="color: #131C63;">Cuidados paliativos são um conjunto de práticas voltadas para melhorar a qualidade de vida de pacientes com doenças graves ou incuráveis.<br><br>
+                O foco não é a cura, mas o alívio do sofrimento, controle da dor e apoio emocional para o paciente e sua família.<br><br>
+                Esse tipo de cuidado pode ser oferecido em qualquer fase da doença, junto a outros tratamentos, e envolve uma equipe multidisciplinar que inclui médicos, enfermeiros, psicólogos e assistentes sociais.
                 </p>
             </div>
-            <img src="../img/4.jpg" alt="Homem sendo vacinado no braço por uma enfermeira feliz" class="ms-5 img-fluid rounded-start" style="width: 40%; box-shadow: 1px 4px 10px #474749;">
+            <img src="../img/4.jpg" alt="Homem sendo vacinado no braço por uma enfermeira feliz" class="ms-5 img-fluid rounded-start" style="width: 40%; box-shadow: 10px 8px 10px #777777;">
         </div>
-            <div id="bordaHome" class="mt-5 container-fluid p-3 card-header text-center text-white">
-                <h2>Nosso objetivo</h2>
-                <p>
-                    Nosso site tem como propósito oferecer informações confiáveis e acessíveis sobre cuidados paliativos. 
-                    Sabemos como é importante entender quando esse tipo de cuidado deve ser considerado, por isso reunimos conteúdos que ajudam a esclarecer dúvidas, dar orientações e facilitar a tomada de decisão.
+        <div class="mt-5 p-5 container-fluid p-3 text-center text-white" style="background-color: #47639C;">
+            <h2>Nosso objetivo</h2>
+            <div style="width: 150px; height: 2px; background-color: white; margin: 0 auto 20px auto;"></div>
+            <p>Nosso site tem como propósito oferecer informações confiáveis e acessíveis sobre cuidados paliativos. 
+            Sabemos como é importante entender quando esse tipo de cuidado deve ser considerado, por isso reunimos conteúdos que ajudam a esclarecer dúvidas, dar orientações e facilitar a tomada de decisão.
+            </p>
+        </div>
+        <div class="mt-5 d-flex align-items-center justify-content-between">
+            <img src="../img/1.jpg" alt="Mãos dadas" class="img-fluid rounded-end" style="width: 40%; box-shadow: -10px 8px 10px #777777;">
+            <div class="mx-5" style="width: 40%">
+                <p style="color: #131C63">Se você ou alguém próximo precisa de cuidados paliativos, saiba que esse tipo de assistência é voltado para <strong>proporcionar conforto, qualidade de vida e apoio integral, tanto para o paciente quanto para a família.</strong><br><br>Os cuidados paliativos podem ser indicados em diversas situações, especialmente em doenças graves ou progressivas, quando o foco passa a ser o bem-estar e o alívio dos sintomas.
                 </p>
             </div>
-            <div class="mt-5 d-flex align-items-center justify-content-between">
-                <img src="../img/1.jpg" alt="" class="img-fluid rounded-end" style="width: 500px; height: auto;">
-                <div class="ms-5">
-                    <p>
-                        Se você ou alguém próximo precisa de cuidados paliativos, saiba que esse tipo de assistência é voltado para <strong>proporcionar conforto, qualidade de vida e apoio integral, tanto para o paciente quanto para a família.</strong>
-                    </p>
-                    <p>
-                        Os cuidados paliativos podem ser indicados em diversas situações, especialmente em doenças graves ou progressivas, quando o foco passa a ser o bem-estar e o alívio dos sintomas.
-                    </p>
-                </div>
-            </div>
+        </div>
             <div class="mt-5">
                 <footer id="bordaHome" >
                     <?php
