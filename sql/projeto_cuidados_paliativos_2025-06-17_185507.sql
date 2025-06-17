3ec9c43d4daa265df96d98490f9b5fe954ca2ec2
Luiza Mariana <luizamarianahg@hotmail.com>
2025-06-17 18:57:10 -0300

    banco

--- sql/projeto_cuidados_paliativos_2025-06-17_185507.sql
+++ sql/projeto_cuidados_paliativos_2025-06-17_185507.sql
@@ -38,16 +38,25 @@ CREATE TABLE `administrador` (
   `ultimoLogin` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `administrador`
+-- Table structure for table `conteudo`
 --
 
-/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
-INSERT INTO `administrador` VALUES (1,'Bananinha kkkk sla','Banoffe','banana@gmail.com','$2y$10$hDgiQgW8VXUwdQE.HBGhI.A5M1hELjgi6WnBnx5VTOzKx3HGU/FqO','1998-09-01','M','COREN','Psicologia','38274/RJ','Trambique','2025-06-04 22:11:29',NULL);
-/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+DROP TABLE IF EXISTS `conteudo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conteudo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `texto` varchar(3000) NOT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `paciente`
@@ -70,16 +79,8 @@ CREATE TABLE `paciente` (
   `doenca` varchar(1000) DEFAULT 'NULL',
   `tipo_sanguineo` varchar(255) DEFAULT 'NULL',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `paciente`
---
-
-/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
-INSERT INTO `paciente` VALUES (8,'João da Silva','joao.silva@email.com','$2y$10$fLnSF8UTAP6yZoHDZqyJFuVhVO0rXg0gbNEwZI7QQQlxVCu.VMIwC','1990-05-20','F','SP','Joana da Silva','São Paulo','Nenhuma','Câncer no reto','O+'),(9,'Maria Oliveira','maria.oliveira@email.com','$2y$10$oXryUmy463xZUeB8EMGssOFTOCVwQFmr.daqAXcpjt3ep5l1/Wo1O','1985-10-12','F','RJ','Mari','Rio de Janeiro','Anticoncepcional','Hipertensão','A+'),(10,'Carla Souza','carla.souza@email.com','$2y$10$GDBY7vuBx/SK7TcLa0Y7qeSCNHLrU1BClpToRJf3/BLMLng8eVq2W','1992-09-03','F','MG','Carla','Belo Horizonte','Antidepressivo','Depressão','B-');
-/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
 
 --
 -- Dumping routines for database 'projeto_cuidados_paliativos'
