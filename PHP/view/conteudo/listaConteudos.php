5acdd8fe34b41264ed0510b966dbfaef82e3b5b2
Luiza Mariana <luizamarianahg@hotmail.com>
2025-06-18 23:20:49 -0300

    retirar o container-height faz o min-height:100vh funcionar :)

--- PHP/view/conteudo/listaConteudos.php
+++ PHP/view/conteudo/listaConteudos.php
@@ -8,12 +8,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conteúdos</title>
+    <link rel="stylesheet" href="listaConteudos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <link rel="stylesheet" href="listagemUsuario.css">
     <link rel="stylesheet" href="../global.css">
 </head>
 <body>
-    <div class="container-fluid p-0">
         <header><?php require '../header/header.php' ?></header>
         <main class="m-5">
             <h1>Lista de Conteúdos</h1>
@@ -34,6 +33,5 @@
             </table>
         </main>
         <footer><?php require '../footer/footer.php' ?></footer>
-    </div>
 </body>
 </html>
