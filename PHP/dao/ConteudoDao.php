a8d478144a1d38f9da8a780fe55fcbb5d196aa67
Luiza Mariana <luizamarianahg@hotmail.com>
2025-06-22 14:19:20 -0300

    consertado ConteudoDao

--- PHP/dao/ConteudoDao.php
+++ PHP/dao/ConteudoDao.php
@@ -1,14 +1,6 @@
 <?php
 
     class ConteudoDao{
-<<<<<<< HEAD:dao/ConteudoDao.php
-        public function inserir(Conteudo $cont){
-            $url = "http://localhost:3000/conteudos";
-            $dados = [
-                "titulo" => $cont->getTitulo(),
-                "descricao" => $cont->getDescricao(),
-                "conteudo" => $cont->getConteudo(),
-=======
         private $conexao;
         
         public function inserir(Conteudo $cont){
@@ -18,7 +10,6 @@
                 "descricao" => $cont->getDescricao(),
                 "texto" => $cont->getTexto(),
                 "categoria" => $cont->getCategoria(),
->>>>>>> a419c745a2a70f6fab0e2c7afa0e586f2e1291c7:PHP/dao/ConteudoDao.php
                 "data" => $cont->getData(),
             ];
 
@@ -36,15 +27,6 @@
         }
     
         public function read(){
-<<<<<<< HEAD:dao/ConteudoDao.php
-            $url = "http://localhost:3000/conteudos";
-            $result = file_get_contents($url);
-            $contList = array();
-            $lista = json_decode($result, true);
-            foreach($lista as $cont):
-                $contList[] = $this->listaConteudo($cont);
-            endforeach;
-=======
             $url = "http://localhost:3001/api/conteudos";
             $result = file_get_contents($url);
             $contList = array();
@@ -55,8 +37,6 @@
                     $contList[] = $this->listaConteudo($cont);
                 }
             }
-            
->>>>>>> a419c745a2a70f6fab0e2c7afa0e586f2e1291c7:PHP/dao/ConteudoDao.php
             return $contList;
         }
 
@@ -65,24 +45,13 @@
             $conteudo->setId(htmlspecialchars($row['id']));
             $conteudo->setTitulo(htmlspecialchars($row['titulo']));
             $conteudo->setDescricao(htmlspecialchars($row['descricao']));
-<<<<<<< HEAD:dao/ConteudoDao.php
-            $conteudo->setConteudo(htmlspecialchars($row['conteudo']));
-=======
             $conteudo->setTexto(htmlspecialchars($row['texto']));
             $conteudo->setCategoria(htmlspecialchars($row['categoria']));
->>>>>>> a419c745a2a70f6fab0e2c7afa0e586f2e1291c7:PHP/dao/ConteudoDao.php
             $conteudo->setData(htmlspecialchars($row['data']));
             return $conteudo;
         }
         
         public function editar(Conteudo $cont){
-<<<<<<< HEAD:dao/ConteudoDao.php
-            $url = "http://localhost:3000/conteudos/".$cont->getId();
-            $dados = [
-                "titulo" => $cont->getTitulo(),
-                "descricao" => $cont->getDescricao(),
-                "conteudo" => $cont->getConteudo(),
-=======
             $url = "http://localhost:3001/api/conteudos";
             $dados = [
                 "id" => $cont->getId(),
@@ -90,7 +59,6 @@
                 "descricao" => $cont->getDescricao(),
                 "texto" => $cont->getTexto(),
                 "categoria" => $cont->getCategoria(),
->>>>>>> a419c745a2a70f6fab0e2c7afa0e586f2e1291c7:PHP/dao/ConteudoDao.php
                 "data" => $cont->getData(),
             ];
 
@@ -112,24 +80,6 @@
             return json_decode($result, true);
         }
     
-<<<<<<< HEAD:dao/ConteudoDao.php
-        public function buscarPorId($id){
-            $url = "http://localhost:3000/conteudos/" . urlencode($id);
-            try {
-                $response = @file_get_contents($url);
-                if ($response === FALSE) {
-                    return null; // ID não encontrado ou erro na requisição
-                }
-                $data = json_decode($response, true);
-                if ($data) {
-                    return $this->listaConteudo($data);
-                }
-                return null;
-            } catch (Exception $e) {
-                echo "<p>Erro ao buscar conteúdo por ID: </p> <p>{$e->getMessage()}</p>";
-                return null;
-            }
-=======
         public function deletar($id) {
             $url = "http://localhost:3001/api/conteudos";
             $dados = ["id" => $id];
@@ -161,7 +111,6 @@
             }
 
             return new Conteudo(); // retorna vazio se não achar
->>>>>>> a419c745a2a70f6fab0e2c7afa0e586f2e1291c7:PHP/dao/ConteudoDao.php
         }
     }
 ?>
